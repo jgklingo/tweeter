9b8e61588283c0a29072c4e7c64be1710cd02b11
jgklingo <jgklingo@byu.edu>
2025-01-31 11:41:49 -0700

    refactor(prettier)

--- tweeter-web/src/components/authentication/AuthenticationFields.tsx
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 interface Props {
-    action: () => void;
+  action: () => void;
 }
 
 const AuthenticationFields = (props: Props) => {
@@ -18,33 +18,33 @@ const AuthenticationFields = (props: Props) => {
     }
   };
 
-    return (
-        <>
-        <div className="form-floating">
-          <input
-            type="text"
-            className="form-control"
-            size={50}
-            id="aliasInput"
-            placeholder="name@example.com"
-            onKeyDown={actionOnEnter}
-            onChange={(event) => setAlias(event.target.value)}
-          />
-          <label htmlFor="aliasInput">Alias</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            type="password"
-            className="form-control bottom"
-            id="passwordInput"
-            placeholder="Password"
-            onKeyDown={actionOnEnter}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-          <label htmlFor="passwordInput">Password</label>
-        </div>
-      </>
-    );
-}
+  return (
+    <>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          size={50}
+          id="aliasInput"
+          placeholder="name@example.com"
+          onKeyDown={actionOnEnter}
+          onChange={(event) => setAlias(event.target.value)}
+        />
+        <label htmlFor="aliasInput">Alias</label>
+      </div>
+      <div className="form-floating mb-3">
+        <input
+          type="password"
+          className="form-control bottom"
+          id="passwordInput"
+          placeholder="Password"
+          onKeyDown={actionOnEnter}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <label htmlFor="passwordInput">Password</label>
+      </div>
+    </>
+  );
+};
 
 export default AuthenticationFields;
--- tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -6,7 +6,7 @@ import { UserInfoContext } from "../userInfo/UserInfoProvider";
 import useToastListener from "../toaster/ToastListenerHook";
 
 interface Props {
-    status: Status
+  status: Status;
 }
 
 const StatusItem = (props: Props) => {
@@ -47,39 +47,39 @@ const StatusItem = (props: Props) => {
     return FakeData.instance.findUserByAlias(alias);
   };
 
-    return (
-        <div className="col bg-light mx-0 px-0">
-        <div className="container px-0">
-          <div className="row mx-0 px-0">
-            <div className="col-auto p-3">
-              <img
-                src={props.status.user.imageUrl}
-                className="img-fluid"
-                width="80"
-                alt="Posting user"
-              />
-            </div>
-            <div className="col">
-              <h2>
-                <b>
-                  {props.status.user.firstName} {props.status.user.lastName}
-                </b>{" "}
-                -{" "}
-                <Link
-                  to={props.status.user.alias}
-                  onClick={(event) => navigateToUser(event)}
-                >
-                  {props.status.user.alias}
-                </Link>
-              </h2>
-              {props.status.formattedDate}
-              <br />
-              <Post status={props.status} />
-            </div>
+  return (
+    <div className="col bg-light mx-0 px-0">
+      <div className="container px-0">
+        <div className="row mx-0 px-0">
+          <div className="col-auto p-3">
+            <img
+              src={props.status.user.imageUrl}
+              className="img-fluid"
+              width="80"
+              alt="Posting user"
+            />
+          </div>
+          <div className="col">
+            <h2>
+              <b>
+                {props.status.user.firstName} {props.status.user.lastName}
+              </b>{" "}
+              -{" "}
+              <Link
+                to={props.status.user.alias}
+                onClick={(event) => navigateToUser(event)}
+              >
+                {props.status.user.alias}
+              </Link>
+            </h2>
+            {props.status.formattedDate}
+            <br />
+            <Post status={props.status} />
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default StatusItem;
