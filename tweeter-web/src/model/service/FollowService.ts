6c5e7285e29e934bd93c9df3bead6423094ffadb
jgklingo <jgklingo@byu.edu>
2025-02-07 15:20:17 -0700

    remove(duplicate methods)

--- tweeter-web/src/model/service/FollowService.ts
+++ tweeter-web/src/model/service/FollowService.ts
@@ -1,4 +1,4 @@
-import { AuthToken, User, FakeData, Status } from "tweeter-shared";
+import { AuthToken, User, FakeData } from "tweeter-shared";
 
 export class FollowService {
     public async loadMoreFollowers(
@@ -19,25 +19,5 @@ export class FollowService {
     ): Promise<[User[], boolean]> {
         // TODO: Replace with the result of calling server
         return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
-    };
-
-    public async loadMoreFeedItems(
-        authToken: AuthToken,
-        userAlias: string,
-        pageSize: number,
-        lastItem: Status | null
-    ): Promise<[Status[], boolean]> {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-    };
-
-    public async loadMoreStoryItems(
-        authToken: AuthToken,
-        userAlias: string,
-        pageSize: number,
-        lastItem: Status | null
-    ): Promise<[Status[], boolean]> {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
     };
 }
