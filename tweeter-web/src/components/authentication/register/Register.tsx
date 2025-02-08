317c0d5a67c81399424887c166de442e0eac9980
jgklingo <jgklingo@byu.edu>
2025-02-07 18:33:38 -0700

    refactor(MVP Register)

--- tweeter-web/src/components/authentication/register/Register.tsx
+++ tweeter-web/src/components/authentication/register/Register.tsx
@@ -3,37 +3,26 @@ import "bootstrap/dist/css/bootstrap.css";
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthenticationFormLayout from "../AuthenticationFormLayout";
-import { AuthToken, FakeData, User } from "tweeter-shared";
-import { Buffer } from "buffer";
 import useToastListener from "../../toaster/ToastListenerHook";
 import AuthenticationFields from "../AuthenticationFields";
 import useUserInfo from "../../userInfo/UserInfoHook";
+import { RegisterPresenter, RegisterView } from "../../../presenters/RegisterPresenter";
 
 const Register = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [alias, setAlias] = useState("");
     const [password, setPassword] = useState("");
-    const [imageBytes, setImageBytes] = useState<Uint8Array>(new Uint8Array());
     const [imageUrl, setImageUrl] = useState<string>("");
-    const [imageFileExtension, setImageFileExtension] = useState<string>("");
     const [rememberMe, setRememberMe] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
 
     const navigate = useNavigate();
     const { updateUserInfo } = useUserInfo();
     const { displayErrorMessage } = useToastListener();
 
     const checkSubmitButtonStatus = (): boolean => {
-        return (
-            !firstName ||
-            !lastName ||
-            !alias ||
-            !password ||
-            !imageUrl ||
-            !imageFileExtension
-        );
-    };
+        return presenter.checkSubmitButtonStatus(firstName, lastName, alias, password, imageUrl)
+    }
 
     const registerOnEnter = (event: React.KeyboardEvent<HTMLElement>) => {
         if (event.key == "Enter" && !checkSubmitButtonStatus()) {
@@ -43,90 +32,21 @@ const Register = () => {
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
-        handleImageFile(file);
-    };
-
-    const handleImageFile = (file: File | undefined) => {
-        if (file) {
-            setImageUrl(URL.createObjectURL(file));
-
-            const reader = new FileReader();
-            reader.onload = (event: ProgressEvent<FileReader>) => {
-                const imageStringBase64 = event.target?.result as string;
-
-                // Remove unnecessary file metadata from the start of the string.
-                const imageStringBase64BufferContents =
-                    imageStringBase64.split("base64,")[1];
-
-                const bytes: Uint8Array = Buffer.from(
-                    imageStringBase64BufferContents,
-                    "base64"
-                );
-
-                setImageBytes(bytes);
-            };
-            reader.readAsDataURL(file);
-
-            // Set image file extension (and move to a separate method)
-            const fileExtension = getFileExtension(file);
-            if (fileExtension) {
-                setImageFileExtension(fileExtension);
-            }
-        } else {
-            setImageUrl("");
-            setImageBytes(new Uint8Array());
-        }
-    };
-
-    const getFileExtension = (file: File): string | undefined => {
-        return file.name.split(".").pop();
+        presenter.handleImageFile(file);
     };
 
     const doRegister = async () => {
-        try {
-            setIsLoading(true);
+        presenter.doRegister(firstName, lastName, alias, password, rememberMe)
+    }
 
-            const [user, authToken] = await register(
-                firstName,
-                lastName,
-                alias,
-                password,
-                imageBytes,
-                imageFileExtension
-            );
+    const listener: RegisterView = {
+        updateUserInfo: updateUserInfo,
+        navigate: navigate,
+        displayErrorMessage: displayErrorMessage,
+        setImageUrl: setImageUrl
+    }
 
-            updateUserInfo(user, user, authToken, rememberMe);
-            navigate("/");
-        } catch (error) {
-            displayErrorMessage(
-                `Failed to register user because of exception: ${error}`
-            );
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    const register = async (
-        firstName: string,
-        lastName: string,
-        alias: string,
-        password: string,
-        userImageBytes: Uint8Array,
-        imageFileExtension: string
-    ): Promise<[User, AuthToken]> => {
-        // Not neded now, but will be needed when you make the request to the server in milestone 3
-        const imageStringBase64: string =
-            Buffer.from(userImageBytes).toString("base64");
-
-        // TODO: Replace with the result of calling the server
-        const user = FakeData.instance.firstUser;
-
-        if (user === null) {
-            throw new Error("Invalid registration");
-        }
-
-        return [user, FakeData.instance.authToken];
-    };
+    const [presenter] = useState(new RegisterPresenter(listener));
 
     const inputFieldGenerator = () => {
         return (
@@ -194,7 +114,7 @@ const Register = () => {
             }
             setRememberMe={setRememberMe}
             submitButtonDisabled={checkSubmitButtonStatus}
-            isLoading={isLoading}
+            isLoading={presenter.isLoading}
             submit={doRegister}
         />
     );
---
+++ tweeter-web/src/presenters/RegisterPresenter.ts
@@ -0,0 +1,117 @@
+import { Buffer } from "buffer";
+import { UserService } from "../model/service/UserService";
+import { User, AuthToken } from "tweeter-shared";
+
+export interface RegisterView {
+    updateUserInfo: (currentUser: User, displayedUser: User | null, authToken: AuthToken, remember: boolean) => void,
+    navigate: (to: string) => void | Promise<void>,
+    displayErrorMessage: (message: string) => void,
+    setImageUrl: (imageUrl: string) => void
+}
+
+export class RegisterPresenter {
+    private userService: UserService;
+    private view: RegisterView;
+    private imageBytes: Uint8Array;
+    private imageFileExtension: string;
+    private _isLoading: boolean;
+
+    public constructor(view: RegisterView) {
+        this.userService = new UserService();
+        this.view = view;
+        this.imageBytes = new Uint8Array();
+        this.imageFileExtension = "";
+        this._isLoading = false;
+    }
+
+    public checkSubmitButtonStatus(
+        firstName: string,
+        lastName: string,
+        alias: string,
+        password: string,
+        imageUrl: string
+    ): boolean {
+        return (
+            !firstName ||
+            !lastName ||
+            !alias ||
+            !password ||
+            !imageUrl ||
+            !this.imageFileExtension
+        );
+    };
+
+    public handleImageFile(file: File | undefined) {
+        if (file) {
+            this.view.setImageUrl(URL.createObjectURL(file));
+
+            const reader = new FileReader();
+            reader.onload = (event: ProgressEvent<FileReader>) => {
+                const imageStringBase64 = event.target?.result as string;
+
+                // Remove unnecessary file metadata from the start of the string.
+                const imageStringBase64BufferContents =
+                    imageStringBase64.split("base64,")[1];
+
+                const bytes: Uint8Array = Buffer.from(
+                    imageStringBase64BufferContents,
+                    "base64"
+                );
+
+                this.imageBytes = bytes;
+            };
+            reader.readAsDataURL(file);
+
+            // Set image file extension (and move to a separate method)
+            const fileExtension = this.getFileExtension(file);
+            if (fileExtension) {
+                this.imageFileExtension = fileExtension;
+            }
+        } else {
+            this.view.setImageUrl("");
+            this.imageBytes = new Uint8Array();
+        }
+    };
+
+    private getFileExtension(file: File): string | undefined {
+        return file.name.split(".").pop();
+    };
+
+    public async doRegister(
+        firstName: string,
+        lastName: string,
+        alias: string,
+        password: string,
+        rememberMe: boolean
+    ) {
+        try {
+            this._isLoading = true;
+
+            const [user, authToken] = await this.userService.register(
+                firstName,
+                lastName,
+                alias,
+                password,
+                this.imageBytes,
+                this.imageFileExtension
+            );
+
+            this.view.updateUserInfo(user, user, authToken, rememberMe);
+            this.view.navigate("/");
+        } catch (error) {
+            this.view.displayErrorMessage(
+                `Failed to register user because of exception: ${error}`
+            );
+        } finally {
+            this._isLoading = false;
+        }
+    };
+
+    public get isLoading() {
+        return this._isLoading;
+    }
+
+    public set isLoading(value: boolean) {
+        this._isLoading = value;
+    }
+}
